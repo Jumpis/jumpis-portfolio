3712c91d1b6a74ce37e05bf1f68515a34aafe33c
Jonghyeon Lee <leejonghyeon0106@gmail.com>
2020-06-26 23:28:42 +0900

    basic setting complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { observer, inject } from "mobx-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+@inject("yourstore")
+@observer
+export default class App extends React.Component<any> {
+  render() {
+    return (
+      <div>
+        <div>value = {this.props.yourstore.yourStore}</div>
+        <div>
+          <button onClick={this.props.yourstore.changeToWorld}>
+            Change to world
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
